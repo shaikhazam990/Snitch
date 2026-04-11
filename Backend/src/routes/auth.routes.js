a75c14769a480a4c3c85c90e080202d277b3832e
shaikhazam990 <shaikhazam0990@gmail.com>
2026-04-11 11:56:05 +0530

    ..

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,8 +1,24 @@
 import { Router } from "express";
-import { validateRegisterUser } from "../validator/auth.validator.js";
+import { validateRegisterUser, validateLoginUser } from "../validator/auth.validator.js";
+import { googleCallback, login, register } from "../controllers/auth.controller.js";
+import passport from "passport";
 
 const router = Router();
 
-router.post('/register', validateRegisterUser)
+
+
+router.post('/register', validateRegisterUser, register)
+
+router.post("/login", validateLoginUser, login)
+
+
+// /api/auth/google
+router.get("/google",
+    passport.authenticate("google", { scope: [ "profile", "email" ] }))
+
+router.get("/google/callback",
+    passport.authenticate("google", { session: false }),
+    googleCallback,
+)
 
 export default router;
