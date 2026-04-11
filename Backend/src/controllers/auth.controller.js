fcebeab363755dcda11c4d47f591d39d05fadcf4
shaikhazam990 <shaikhazam0990@gmail.com>
2026-04-11 11:54:59 +0530

    working on auth

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,16 +1,35 @@
-import userModel from "../models/user.model";
-import jwt from "jsonwebtoken";
+import userModel from "../models/user.model.js";
+import jwt from "jsonwebtoken"
 import { config } from "../config/config.js";
 
-async function sendTokenResponse(user,res) {
+
+async function sendTokenResponse(user, res, message) {
+
     const token = jwt.sign({
         id: user._id,
-    }, config.JWT_SECRET)
+    }, config.JWT_SECRET, {
+        expiresIn: "9d"
+    })
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message,
+        success: true,
+        user: {
+            id: user._id,
+            email: user.email,
+            contact: user.contact,
+            fullname: user.fullname,
+            role: user.role
+        }
+    })
 
 }
 
-export const register = async (req,res) =>{
-    const {email,contact,password,fullname}= req.body;
+
+export const register = async (req, res) => {
+    const { email, contact, password, fullname, isSeller } = req.body;
 
     try {
         const existingUser = await userModel.findOne({
@@ -19,6 +38,7 @@ export const register = async (req,res) =>{
                 { contact }
             ]
         })
+
         if (existingUser) {
             return res.status(400).json({ message: "User with this email or contact already exists" });
         }
@@ -27,12 +47,38 @@ export const register = async (req,res) =>{
             email,
             contact,
             password,
-            fullname
+            fullname,
+            role: isSeller ? "seller" : "buyer"
         })
+
+        await sendTokenResponse(user, res, "User registered successfully")
+
     } catch (error) {
         console.log(error)
-        return res.status(500).json({message: "server error"})
-        
+        return res.status(500).json({ message: "Server error" });
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+        return res.status(400).json({ message: "Invalid email or password" });
     }
 
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+        return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    await sendTokenResponse(user, res, "User logged in successfully")
+}
+
+export const googleCallback = async (req, res) => {
+    console.log(req.user)
+
+    res.redirect("http://localhost:5173/")
 }
