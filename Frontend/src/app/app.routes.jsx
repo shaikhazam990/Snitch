9d7a9ae78b7f223874813695759184a751640017
shaikhazam990 <shaikhazam0990@gmail.com>
2026-07-08 13:19:31 +0530

    deploy frontend

--- Frontend/src/app/app.routes.jsx
+++ Frontend/src/app/app.routes.jsx
@@ -8,7 +8,7 @@ import Home from "../features/products/pages/Home";
 import ProductDetail from "../features/products/pages/ProductDetail";
 import SellerProductDetails from "../features/products/pages/SellerProductDetails";
 import Cart from "../features/cart/pages/Cart";
-import AppLayout from "./Applayout";
+import AppLayout from "./AppLayout";
 import OrderSuccess from "../features/cart/pages/OrderSuccess";
 
 export const routes = createBrowserRouter([
